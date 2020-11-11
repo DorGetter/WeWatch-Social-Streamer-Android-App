0ef8bd552b3d1b5a26426d9cef58af39f631d78f
DorGetter <57187365+DorGetter@users.noreply.github.com>
2020-11-12 00:43:11 +0200

    build user vod cont.

--- app/src/main/java/com/example/wewatchapp/userPack/Vod.java
+++ app/src/main/java/com/example/wewatchapp/userPack/Vod.java
@@ -1,6 +1,8 @@
 package com.example.wewatchapp.userPack;
 
 import android.app.ProgressDialog;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,7 @@ import com.example.wewatchapp.R;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.database.DatabaseReference;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Vod extends AppCompatActivity {
@@ -32,6 +35,63 @@ public class Vod extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vod);
+
+        inViews();
+        moviesViewTab();
+    }
+
+    private void addAllMovies(){
+        uploads = new ArrayList<>();
+        uploadlistLatest = new ArrayList<>();
+        uploadListPopular = new ArrayList<>();
+        actionMovies = new ArrayList<>();
+        adventureMovies = new ArrayList<>();
+        comedyMovies = new ArrayList<>();
+        sportMovies = new ArrayList<>();
+        romanticMovies = new ArrayList<>();
+        uploadSlider = new ArrayList<>();
+
+
+    }
+
+
+    private void moviesViewTab(){
+        tabMovieActions.addTab(tabMovieActions.newTab().setText("action"));
+        tabMovieActions.addTab(tabMovieActions.newTab().setText("adventure"));
+        tabMovieActions.addTab(tabMovieActions.newTab().setText("comedy"));
+        tabMovieActions.addTab(tabMovieActions.newTab().setText("Romance"));
+        tabMovieActions.addTab(tabMovieActions.newTab().setText("Sports"));
+        tabMovieActions.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabMovieActions.setTabTextColors(ColorStateList.valueOf(Color.WHITE));
+
+        tabMovieActions.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch(tab.getPosition()){
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 
     private void inViews(){
