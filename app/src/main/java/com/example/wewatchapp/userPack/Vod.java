ba9aec62d8713d2f4bdf0f29ce0df08450f3cfb3
DorGetter <57187365+DorGetter@users.noreply.github.com>
2020-11-12 15:13:07 +0200

    change api level- to unable progressDialog.

--- app/src/main/java/com/example/wewatchapp/userPack/Vod.java
+++ app/src/main/java/com/example/wewatchapp/userPack/Vod.java
@@ -4,23 +4,34 @@ import android.app.ProgressDialog;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.wewatchapp.Adapter.MoviesShowAdapter;
+import com.example.wewatchapp.Adapter.SliderPagerAdapterNew;
 import com.example.wewatchapp.Model.GetVideoDetails;
+import com.example.wewatchapp.Model.MoviesItemClickListenerNew;
 import com.example.wewatchapp.Model.SliderSide;
 import com.example.wewatchapp.R;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Vod extends AppCompatActivity {
+public class Vod extends AppCompatActivity implements MoviesItemClickListenerNew {
 
     MoviesShowAdapter moviesShowAdapter;
     DatabaseReference mDatabaseReference;
@@ -37,10 +48,25 @@ public class Vod extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vod);
 
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.actionbar);
+
+        progressDialog = new ProgressDialog(this);
+
         inViews();
+        addAllMovies();
+        iniPopularMovies();
+        iniWeekMovies();
         moviesViewTab();
+
+
+
     }
 
+
+    /**
+     * addinf movies from dataBase
+     */
     private void addAllMovies(){
         uploads = new ArrayList<>();
         uploadlistLatest = new ArrayList<>();
@@ -54,15 +80,96 @@ public class Vod extends AppCompatActivity {
 
         mDatabaseReference = FirebaseDatabase.getInstance().getReference("videos");
         progressDialog.setMessage("loading...");
+        progressDialog.show();
+
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+
+                    GetVideoDetails upload = postSnapshot.getValue(GetVideoDetails.class);
+                    SliderSide slide = postSnapshot.getValue(SliderSide.class);
+
+                    //getting to the label
+                    if(upload.getVideo_type().equals("latest movies")){
+                        uploadlistLatest.add(upload);
+                    }
+                    if(upload.getVideo_type().equals("Best popular movies")){
+                        uploadListPopular.add(upload);
+                    }
+
+                    if(upload.getVideo_type().equals("Slide movies")){
+                        uploadSlider.add(slide);
+                    }
+
+                    if(upload.getVideo_category().equals("Action")){
+                        actionMovies.add(upload);
+                    }
+
+                    if(upload.getVideo_category().equals("adventure")){
+                        adventureMovies.add(upload);
+                    }
+
+                    if(upload.getVideo_category().equals("romantic")){
+                        romanticMovies.add(upload);
+                    }
+                    if(upload.getVideo_category().equals("sports")){
+                        sportMovies.add(upload);
+                    }
+
+                    //                    if(upload.getVideo_type().equals("No Type")){
+//                        .add(upload);
+//                    }
+
+                    uploads.add(upload);
+                }
+                iniSlider();
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 
+    ///movies sliders  and tabs.
+    private void iniSlider() {
+        SliderPagerAdapterNew adapterNew = new SliderPagerAdapterNew(this,uploadSlider);
+        slidePager.setAdapter(adapterNew);
+
+        //setup timer:
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new SliderTimer(), 4000, 6000);
+        indicator.setupWithViewPager(slidePager,true);
+
+    }
+
+    private void iniWeekMovies(){
+        moviesShowAdapter = new MoviesShowAdapter(this,uploadlistLatest,this);
+        moviesRvWeek.setAdapter(moviesShowAdapter);
+        moviesRvWeek.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false));
+        moviesShowAdapter.notifyDataSetChanged();
+    }
+
+    private void iniPopularMovies(){
+        moviesShowAdapter = new MoviesShowAdapter(this,uploadListPopular,this);
+        moviesRv.setAdapter(moviesShowAdapter);
+        moviesRv.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false));
+        moviesShowAdapter.notifyDataSetChanged();
+    }
+
+
+
     private void moviesViewTab(){
+        getActionMovies();
         tabMovieActions.addTab(tabMovieActions.newTab().setText("action"));
         tabMovieActions.addTab(tabMovieActions.newTab().setText("adventure"));
         tabMovieActions.addTab(tabMovieActions.newTab().setText("comedy"));
-        tabMovieActions.addTab(tabMovieActions.newTab().setText("Romance"));
-        tabMovieActions.addTab(tabMovieActions.newTab().setText("Sports"));
+        tabMovieActions.addTab(tabMovieActions.newTab().setText("romance"));
+        tabMovieActions.addTab(tabMovieActions.newTab().setText("sports"));
         tabMovieActions.setTabGravity(TabLayout.GRAVITY_FILL);
         tabMovieActions.setTabTextColors(ColorStateList.valueOf(Color.WHITE));
 
@@ -71,14 +178,19 @@ public class Vod extends AppCompatActivity {
             public void onTabSelected(TabLayout.Tab tab) {
                 switch(tab.getPosition()){
                     case 0:
+                        getActionMovies();
                         break;
                     case 1:
+                        getAdventureMovies();
                         break;
                     case 2:
+                        getComedyMovies();
                         break;
                     case 3:
+                        getRomanticMovies();
                         break;
                     case 4:
+                        getSportMovies();
                         break;
                 }
             }
@@ -105,4 +217,63 @@ public class Vod extends AppCompatActivity {
         tab = findViewById(R.id.tabrecycler);
 
     }
+
+    @Override
+    public void onMovieClick(GetVideoDetails getVideoDetails, ImageView imageView) {
+
+    }
+
+    /**
+     * Slider moving video timer inner class
+     */
+    public class SliderTimer extends TimerTask {
+        public void run(){
+
+            Vod.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(slidePager.getCurrentItem()<uploadSlider.size()-1){
+                        slidePager.setCurrentItem(slidePager.getCurrentItem()+1);
+                    }else{                        slidePager.setCurrentItem(0); }
+                }
+            });
+        }
+    }
+
+    private void getActionMovies(){
+        moviesShowAdapter = new MoviesShowAdapter(this,actionMovies,this);
+        tab.setAdapter(moviesShowAdapter);
+        tab.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false));
+        moviesShowAdapter.notifyDataSetChanged();
+    }
+
+    private void getSportMovies(){
+        moviesShowAdapter = new MoviesShowAdapter(this,sportMovies,this);
+        tab.setAdapter(moviesShowAdapter);
+        tab.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false));
+        moviesShowAdapter.notifyDataSetChanged();
+    }
+
+    private void getRomanticMovies(){
+        moviesShowAdapter = new MoviesShowAdapter(this,romanticMovies,this);
+        tab.setAdapter(moviesShowAdapter);
+        tab.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false));
+        moviesShowAdapter.notifyDataSetChanged();
+    }
+
+    private void getComedyMovies(){
+        moviesShowAdapter = new MoviesShowAdapter(this,comedyMovies,this);
+        tab.setAdapter(moviesShowAdapter);
+        tab.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false));
+        moviesShowAdapter.notifyDataSetChanged();
+    }
+
+    private void getAdventureMovies(){
+        moviesShowAdapter = new MoviesShowAdapter(this,adventureMovies,this);
+        tab.setAdapter(moviesShowAdapter);
+        tab.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false));
+        moviesShowAdapter.notifyDataSetChanged();
+    }
+
+
 }
