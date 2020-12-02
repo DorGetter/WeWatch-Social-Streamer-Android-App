6d98668c271f068350a16f4714c54c7d4221c5b7
DorGetter <57187365+DorGetter@users.noreply.github.com>
2020-12-02 18:09:26 +0200

    my activity

--- app/src/main/java/com/example/wewatchapp/userPack/MyActivity.java
+++ app/src/main/java/com/example/wewatchapp/userPack/MyActivity.java
@@ -2,35 +2,92 @@ package com.example.wewatchapp.userPack;
 
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.wewatchapp.R;
+import com.example.wewatchapp.utilitiesPack.Views;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
+public class MyActivity extends AppCompatActivity implements View.OnClickListener {
 
-
-
-public class MyActivity extends AppCompatActivity {
     private TextView text;
+
+    /* firebase object */
+    FirebaseDatabase database;
+    /* firebase reference to the root */
+    DatabaseReference rootRef;
+
+    /* use to store views from firebase */
+    final Views[] views = new Views[6];
+
+    private FirebaseUser user;
+    private DatabaseReference reference
+            = FirebaseDatabase.getInstance().getReference("Users");
+
+    String userName = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_activity);
         text = findViewById(R.id.activity);
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setCustomView(R.layout.actionbar);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        rootRef = FirebaseDatabase.getInstance().getReference();
+
+        reference.child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile    = snapshot.getValue(User.class);
+                if(  userProfile   != null){
+                    userName = userProfile.getFullName();
+
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        /* database requests listener */
+        rootRef.child("Views").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot child : snapshot.getChildren()){
+                    Views views = child.getValue(Views.class);
+                    if(views.getUserName().equals(userName)){
+                        System.out.println(views.toString());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
 
     }
 
 
+    @Override
+    public void onClick(View v) {
 
-
-
-
+    }
 }
 
 
